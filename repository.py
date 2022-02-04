aa5d602595dfc82d69fe87e018fe6aae31681a9b
Erik Dzul <erikdzul@outlook.com>
2022-02-03 19:02:27 -0600

    Repository retrieve batch with allocations

--- repository.py
+++ repository.py
@@ -27,4 +27,31 @@ class SqlRepository(AbstractRepository):
 
     def get(self, reference) -> model.Batch:
         # self.session.execute('SELECT ??
-        ...
+        batch_row = self.session.execute(
+            'SELECT id, reference, sku, _purchased_quantity, eta from batches where reference=:reference',
+            dict(reference=reference)
+        ).fetchone()
+
+        if batch_row:
+            batch = model.Batch(
+                ref=batch_row.reference,
+                sku=batch_row.sku,
+                qty=batch_row._purchased_quantity,
+                eta=batch_row.eta
+            )
+
+            allocations = list(self.session.execute(
+                'SELECT orderid, sku, qty '
+                ' FROM allocations '
+                ' JOIN order_lines on allocations.orderline_id = order_lines.id'
+                ' WHERE allocations.batch_id=:batch_id',
+                dict(batch_id=batch_row.id))
+            )
+
+            batch._allocations = {model.OrderLine(o.orderid, o.sku, o.qty)
+                                    for o in allocations}
+        
+        else:
+            batch = None
+
+        return batch
